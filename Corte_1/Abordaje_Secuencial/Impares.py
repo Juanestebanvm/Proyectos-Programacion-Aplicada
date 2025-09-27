93493a00178e7a42adcd87e98de35e590cfc21be
Juanestebanvm <juevargasm@udistrital.edu.co>
2025-09-26 20:31:05 -0500

    Update Impares.py

--- Corte_1/Abordaje_Secuencial/Impares.py
+++ Corte_1/Abordaje_Secuencial/Impares.py
@@ -1,1 +1,18 @@
+#Este Ejercicio Vale una Decima en la Calificacion del Primer Corte
+#Juan Vargas 20232005027
+while True:
+    try:
+        entrada = input("Introduce un número entero (o deja vacío para salir): ")
+        if not entrada:
+            break
+        numero = int(entrada)
+        if numero % 2 == 0:
+            print(f"{numero} es PAR")
+        else:
+            print(f"{numero} es IMPAR")
 
+    except ValueError:
+        print("Entrada no válida. Por favor, introduce un número entero.")
+    except KeyboardInterrupt:
+        print("\nPrograma interrumpido por el usuario. ¡Adiós!")
+        break
