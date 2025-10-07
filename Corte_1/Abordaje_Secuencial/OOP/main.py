475543d2d29fd85cc3e4ba479ef9b78a5c7a4ba2
Juanestebanvm <juevargasm@udistrital.edu.co>
2025-10-07 01:01:30 -0500

    Update main.py

--- Corte_1/Abordaje_Secuencial/OOP/main.py
+++ Corte_1/Abordaje_Secuencial/OOP/main.py
@@ -1,1 +1,27 @@
+#Este Ejercicio Vale una unidad en la Calificacion del Primer Corte
+#Codigo Usando try except
+#Juan Vargas 20232005027
+import ClaseOperaciones
 
+try:
+    obj = ClaseOperaciones.OperacionesBasicas()
+    obj.ingresarValores()
+
+    # Suma
+    obj.sumar(obj.getVal(), obj.getVal2())
+    print("Suma: ", obj.getR())
+
+    # Resta
+    obj.restar(obj.getVal(), obj.getVal2())
+    print("Resta: ", obj.getR())
+
+    # División
+    obj.dividir(obj.getVal(), obj.getVal2())
+    print("División: ", obj.getR())
+
+    # Multiplicación
+    obj.multiplicar(obj.getVal(), obj.getVal2())
+    print("Multiplicación: ", obj.getR())
+
+except Exception as e:
+    print("Ocurrió un error inesperado:", e)
